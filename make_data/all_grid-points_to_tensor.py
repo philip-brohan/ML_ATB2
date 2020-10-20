0671d7767a5b7471c50e5f29c7a3b32c68cfbb16
Philip Brohan <philip@brohan.org>
2020-10-20 16:45:52 +0100

    Added tensor conversion scripts for grid-cell corner metadata.

---
+++ make_data/all_grid-points_to_tensor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Convert all the fake rainfall image grid-corner locations to tensors
+#  for ML model training
+
+# This script does not run the commands - it makes a list of commands
+#  (in the file 'run_g2t.txt') which can be run in parallel.
+
+import os
+
+rootd = "%s/OCR-fake/meta/" % os.getenv("SCRATCH")
+
+
+f = open("run_g2t.sh", "w+")
+
+for doci in range(10000):
+    if os.path.isfile(
+        "%s/ML_ATB2/tensors/cell-corners/%04d.tfd" % (os.getenv("SCRATCH"), doci)
+    ):
+        continue
+    cmd = ('./metadata_to_grid_tensor.py --docn="%04d"\n') % doci
+    f.write(cmd)
+
+f.close()
